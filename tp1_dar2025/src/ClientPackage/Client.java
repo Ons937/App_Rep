8e36359f414143265965fc88b0c42308f1664f42
LENOVO <LENOVO@DESKTOP-HDSR4K8>
2025-10-06 18:49:27 +0100

    Tp1-2

--- tp1_dar2025/src/ClientPackage/Client.java
+++ tp1_dar2025/src/ClientPackage/Client.java
@@ -1,16 +1,25 @@
 package ClientPackage;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
+import java.util.Scanner;
+import java.net.*;
 public class Client {
 
 	public static void main(String[] args) throws  IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Je suis un client en attent de connectee..");
 		Socket socket = new Socket("localhost", 1234);
-		System.out.println("Je suis un client connecte");
+		Scanner sc=new Scanner(System.in);
+		OutputStream os = socket.getOutputStream();
+		System.out.println("entre un entier");
+		os.write(sc.nextInt());
+		InputStream is = socket.getInputStream();
+		int nb=is.read();
+		System.out.println("la resultat est :"+nb);
 		socket.close();
 	}
 
--- tp1_dar2025/src/serverPackage/Server.java
+++ tp1_dar2025/src/serverPackage/Server.java
@@ -1,18 +1,33 @@
 package serverPackage;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
-		ServerSocket socketServeur = new ServerSocket(1234);
-		System.out.println("Je suis un serveur en attente la connexion d'un client");
-		Socket socket = socketServeur.accept();
-		System.out.println("un client est connecte");
-		socket.close();
+
+		try {
+			ServerSocket socketServeur = new ServerSocket(1234);
+			System.out.println("Je suis un serveur en attente la connexion d'un client");
+			Socket socket = socketServeur.accept();
+			System.out.println("un client est connecte");
+			InputStream is = socket.getInputStream();
+			int nb=is.read();
+			System.out.println("la valeur reçu est: "+ nb);
+			int x=nb*5;
+			OutputStream os =socket.getOutputStream();
+			os.write(x);
+			socket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 
